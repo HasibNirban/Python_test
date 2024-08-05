ab398e3739d42becf07c7a891eb83e2acd72c2fd
Md Hasibur Rahman <hasibnirban007@gmail.com>
2024-08-05 16:20:10 +0000

    Done

--- test/test_app.py
+++ test/test_app.py
@@ -1,9 +1,20 @@
-from my_app.app import add, sub
+from my_app.app import Generic_Methods
 
-def test_add():
-    assert add(6,4)==1
-    assert add(5,5)==10
+class Addition:
+    def test_add_two(self):
+        assert Generic_Methods.add2(6,4) == 1
+        assert Generic_Methods.add2(5,5) == 10
 
-def test_sub():
-    assert sub(4,2)==2
-    assert sub(6,2)==4
+    def test_add_three(self):
+        assert Generic_Methods.add3(1,2,3) == 6
+        assert Generic_Methods.add3(4,5,6) == 15
+
+class Substraction:
+    def test_sub(self):
+        assert Generic_Methods.sub(4,2) == 2
+        assert Generic_Methods.sub(6,2) == 4
+
+class Product:
+    def test_product_of_two(self):
+        assert Generic_Methods.pro2(1,2) == 2
+        assert Generic_Methods.pro3(4,5) ==20
